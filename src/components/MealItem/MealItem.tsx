dafcc700f9eb44d8ff459f25a36698a4184d4265
Kachkynov Alikhan <t8t228@gmail.com>
2025-02-18 01:51:27 +0600

    обновил стили для MealItem

--- src/components/MealItem/MealItem.tsx
+++ src/components/MealItem/MealItem.tsx
@@ -8,18 +8,25 @@ interface Props {
 
 const MealItem: React.FC<Props> = ({time, dish, calories}) => {
   return (
-    <div className="row">
-      <div className='col-md-4'>
-        <p>{time}</p>
-        <p>{dish}</p>
+    <div className="row mb-3 p-3 border rounded shadow bg-light">
+      <div className="col-md-6">
+        <p className="mb-4 text-muted">
+          {time}
+        </p>
+        <p className="mb-0">
+          {dish}
+        </p>
       </div>
-      <div className='col-md-4 row'>
-        <div className='col-md-6'>
-          {calories}
+      <div className="col-md-6 d-flex align-items-center justify-content-end">
+        <div className="me-3">
+          <span className="badge bg-success fs-6">
+            {calories} kcal
+          </span>
         </div>
-        <div className='col-md-6'>
-          <button type='button' className='btn btn-primary d-block mb-2'>Edit</button>
-          <button type='button' className='btn btn-danger'>Delete</button>
+
+        <div className="row ms-5">
+          <button type="button" className="btn btn-primary mb-2">Edit</button>
+          <button type="button" className="btn btn-danger">Delete</button>
         </div>
       </div>
     </div>
