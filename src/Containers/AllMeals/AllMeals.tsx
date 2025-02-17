64077a01970839636e4a953e3d90ba02c540b4fc
Kachkynov Alikhan <t8t228@gmail.com>
2025-02-18 01:59:24 +0600

    добавил прелоадер

--- src/Containers/AllMeals/AllMeals.tsx
+++ src/Containers/AllMeals/AllMeals.tsx
@@ -2,12 +2,15 @@ import { useCallback, useEffect, useState } from "react";
 import axiosApi from "../../axiosApi.ts";
 import { IMeal, IMealApi } from "../../types";
 import MealItem from "../../components/MealItem/MealItem.tsx";
+import Loader from "../../UI/Loader.tsx";
 
 const AllMeals = () => {
   const [meals, setMeals] = useState<IMeal[]>([]);
+  const [loader, setLoader] = useState<boolean>(false);
 
   const fetchAllMeals = useCallback(async () => {
     try {
+      setLoader(true);
       const response = await axiosApi<IMealApi>("meals.json");
       if (response.data) {
         const objMeals = response.data;
@@ -25,6 +28,8 @@ const AllMeals = () => {
       }
     } catch (e) {
       console.error(e);
+    } finally {
+      setLoader(false);
     }
   }, []);
 
@@ -32,12 +37,24 @@ const AllMeals = () => {
     void fetchAllMeals();
   }, [fetchAllMeals]);
 
+  const mealsContent = (
+    <>
+      {meals.length ?
+        meals.map((meal) => (<MealItem time={meal.time} key={meal.id} dish={meal.text} calories={meal.calories}/>)) :
+        <p>No meals yet</p>
+      }
+    </>
+  );
+
+  const content = (
+    <>
+      {loader ? <Loader/> : mealsContent}
+    </>
+  );
+
   return (
     <div>
-      {meals.length ?
-        meals.map((meal) => (<MealItem time={meal.time} key={meal.id} dish={meal.text} calories={meal.calories} />)) :
-        <p>No quotes yet</p>
-      }
+      {content}
     </div>
   );
 };
